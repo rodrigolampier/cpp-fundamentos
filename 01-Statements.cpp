613d3e99f2339ccb5fac9139e13516ca98405d9e
= <=>
2021-09-07 18:28:53 -0300

    Adição do arquivo sobre instruções

---
+++ 01-Statements.cpp
@@ -0,0 +1,29 @@
+/*
+Um statement é uma instrução ou uma sequência de instruções em C++,
+que faz com que o programa execute alguma ação.
+A maioria (mas não todos) os statements em C++ termina em ponto-e-vírgula.
+São a menor unidade de computação independente na linguagem C++.
+Em uma linguagem de alto nível como C++, um único statement pode ser compilado em várias instruções de linguagem de máquina.
+
+Existem muitos tipos diferentes de statements em C++:
+Statements de declaração
+Statements de jump
+Statements de expressão
+Statements compostas
+Statements condicionais
+Statements de iteração (loops)
+Statements de blocos try
+
+Os statements geralmente são agrupados em funções.
+Uma função é uma coleção de instruções que são executadas sequencialmente.
+*/
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    cout << "Olá mundo!!!" << endl;
+    return 0;
+}
